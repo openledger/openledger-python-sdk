6a5bad73d5c25c3dd2097abfd11d374557dc00c0
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-19 16:04:49 +0000

    Release 1.0.3

--- src/openledger/core/client_wrapper.py
+++ src/openledger/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "OpenLedger",
-            "X-Fern-SDK-Version": "1.0.2",
+            "X-Fern-SDK-Version": "1.0.3",
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
         return headers
